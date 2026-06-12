e542a8269a60dcc53c020c5c9a5f0f2030e6e60f
Aditya <Aditya@DESKTOP-ELG7MAT>
2026-06-12 13:37:57 +0530

    Adding new Project

---
+++ TestWithMaven/src/test/java/JavaPackage/TestWebDriverManager.java
@@ -0,0 +1,24 @@
+package JavaPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestWebDriverManager {
+
+	@Test
+	public void a() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.paytm.com/");
+		
+	}
+
+}
